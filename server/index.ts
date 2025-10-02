cc77fab1ab596022b0dee7946cf1b51a14b7bc43
voycebutler18 <voycebutler1@gmail.com>
2025-10-02 11:24:01 -0500

    Create index.ts

---
+++ server/index.ts
@@ -0,0 +1,15 @@
+{
+  "compilerOptions": {
+    "target": "ES2020",
+    "module": "ES2020",
+    "moduleResolution": "Bundler",
+    "outDir": "dist",
+    "rootDir": "src",
+    "esModuleInterop": true,
+    "forceConsistentCasingInFileNames": true,
+    "strict": true,
+    "skipLibCheck": true
+  },
+  "include": ["src/**/*"],
+  "exclude": ["dist", "node_modules"]
+}
