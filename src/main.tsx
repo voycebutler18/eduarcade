6ccf857c5e0114a97af33fcd7c81c558330b3123
voycebutler18 <voycebutler1@gmail.com>
2025-10-02 10:35:22 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import "./index.css";
+// Global styles
+import "./styles/app.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
